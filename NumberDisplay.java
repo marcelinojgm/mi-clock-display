585808994e6109b4171a49f4e3bf0abce5984a78
MJGM <MJGM@example.com>
2014-11-12 11:15:07 +0100

    primer commit creacion clase NumberDisplay

---
+++ NumberDisplay.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class NumberDisplay here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class NumberDisplay
+{
+    // instance variables - replace the example below with your own
+    
+
+    /**
+     * Constructor for objects of class NumberDisplay
+     */
+    public NumberDisplay()
+    {
+        
+    }
+
+   
+}
